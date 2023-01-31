99cec19afc9d8266f47b033a9c8c61269197cda3
MarioRivis <mario.rivis@gmail.com>
2023-02-01 01:51:27 +0200

    removed redis test

--- __tests__/redis.integration.test.ts
+++ __tests__/redis.integration.test.ts
@@ -1,16 +1,16 @@
-// import {redisDownAction, redisInfoAction, redisInitAction, redisUpAction} from '../src/commands/redis'
-//
-// describe('test redis commands', () => {
-//     it('test redis info', async () => {
-//         redisInfoAction()
-//     })
-//     it('test redis init', async () => {
-//         redisInitAction()
-//     })
-//     it('test redis up', async () => {
-//         await redisUpAction()
-//     })
-//     it('test redis down', async () => {
-//         await redisDownAction()
-//     })
-// })
+import {redisDownAction, redisInfoAction, redisInitAction, redisUpAction} from '../src/commands/redis'
+
+describe('test redis commands', () => {
+    // it('test redis info', async () => {
+    //     redisInfoAction()
+    // })
+    // it('test redis init', async () => {
+    //     redisInitAction()
+    // })
+    // it('test redis up', async () => {
+    //     await redisUpAction()
+    // })
+    // it('test redis down', async () => {
+    //     await redisDownAction()
+    // })
+})
