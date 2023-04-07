718e1a1def79ccaaaf686b7d49cf68ee29cfb4ce
MarioRivis <mario.rivis@gmail.com>
2023-04-07 18:17:09 +0300

    Fixed blacklist issue

--- src/utils/blacklist.ts
+++ src/utils/blacklist.ts
@@ -3,4 +3,6 @@ import fs from 'fs'
 
 const blacklistFile = path.join(process.cwd(), '.blacklist')
 
-export const blacklistedGlobs = fs.readFileSync(blacklistFile).toString().split('\n').filter(it => it.trim() !== '' && !it.startsWith('#'))
+export const blacklistedGlobs = fs.existsSync(blacklistFile) ?
+    fs.readFileSync(blacklistFile).toString().split('\n').filter(it => it.trim() !== '' && !it.startsWith('#'))
+    : []
