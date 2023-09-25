b915b60c9b661e70dc46b8f9fbf000a9a1410f86
MarioRivis <mario.rivis@gmail.com>
2023-09-25 09:48:16 +0300

    Commented out tests that are only local.

--- __tests__/analyse.integration.test.ts
+++ __tests__/analyse.integration.test.ts
@@ -2,10 +2,10 @@ import {analyseFiles} from '../src/commands/analyse'
 
 describe('test analyse for default plugins', () => {
     it('test analyse for javascript and ruby', async () => {
-
-        await analyseFiles(['/Users/mario/test-projects/depinder/dxworks'], {results:'results-test-mongo', refresh: false,
-            plugins: ['.net']})
-
-        console.log('done')
+        //
+        // await analyseFiles(['/Users/mario/test-projects/depinder/dxworks'], {results:'results-test-mongo', refresh: false,
+        //     plugins: ['.net']})
+        //
+        // console.log('done')
     }, 7200000)
 })
--- __tests__/init.test.ts
+++ __tests__/init.test.ts
@@ -2,8 +2,8 @@ import {walkDir} from '../src/utils/utils'
 
 describe('walk dir test', () => {
     it('should walk the entire dir', () => {
-        const files = walkDir('.')
-
-        expect(files.length).toBeGreaterThan(20)
+        // const files = walkDir('.')
+        //
+        // expect(files.length).toBeGreaterThan(20)
     })
 })
