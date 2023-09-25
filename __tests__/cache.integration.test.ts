46edccc0b87ebca9b3b76879f4ab403cd8c2f616
MarioRivis <mario.rivis@gmail.com>
2023-09-25 10:10:38 +0300

    Cleaned up more tests

--- __tests__/cache.integration.test.ts
+++ __tests__/cache.integration.test.ts
@@ -1,16 +1,16 @@
-// import {cacheDownAction, cacheInfoAction, cacheInitAction, cacheUpAction} from '../src/commands/cache'
-//
-// describe('test cache commands', () => {
-//     it('test cache info', async () => {
-//         cacheInfoAction()
-//     })
-//     it('test cache init', async () => {
-//         cacheInitAction()
-//     })
-//     it('test cache up', async () => {
-//         await cacheUpAction()
-//     })
-//     it('test cache down', async () => {
-//         await cacheDownAction()
-//     })
-// })
+import {cacheDownAction, cacheInfoAction, cacheInitAction, cacheUpAction} from '../src/commands/cache'
+
+describe('test cache commands', () => {
+    it('test cache info', async () => {
+        // cacheInfoAction()
+    })
+    it('test cache init', async () => {
+        // cacheInitAction()
+    })
+    it('test cache up', async () => {
+        // await cacheUpAction()
+    })
+    it('test cache down', async () => {
+        // await cacheDownAction()
+    })
+})
